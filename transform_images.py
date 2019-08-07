90f352663bc7906d7d066a311bd416178f2927a7
Adrian Roth <adrianroth10@gmail.com>
2019-08-07 10:46:43 +0200

    yeah baby

--- transform_images.py
+++ transform_images.py
@@ -6,23 +6,29 @@ import matplotlib.pyplot as plt
 import numpy as np
 import sys
 
-def transform_images():
-    pts_dst = points[0]
-    shape_dst = loaded_images[0].shape
-    for i in range(1, len(images)):
-        h, status = cv2.findHomography(points[i], pts_dst)
-        print(i)
-        print(h)
-        im_out = cv2.warpPerspective(loaded_images[i], h, (shape_dst[1], shape_dst[0]))
+def transform_images(hs=None):
+    if hs is None:
+        pts_dst = points[0]
+        hs = np.zeros((3 * len(images), 3))
+        hs[0:3, :] = np.eye(3)
+        for i in range(1, len(images)):
+            h, status = cv2.findHomography(points[i], pts_dst)
+            hs[3*i:3*(i + 1), :] = h
 
+        np.savetxt(osp.join(folder, 'homographies.txt'), hs)
+
+    shape_dst = loaded_images[0].shape
+    for i in range(len(images)):
+        h = hs[3*i:3*(i+1), :]
+        im_out = cv2.warpPerspective(loaded_images[i], h, (shape_dst[1], shape_dst[0]))
         directory, filename = osp.split(images[i])
         cv2.imwrite(osp.join(directory, 'transformed_' + filename), im_out)
 
 def onclick(event):
     global i, j
     ix, iy = event.xdata, event.ydata
-    points[i, j, 0] = iy
-    points[i, j, 1] = ix
+    points[i, j, 0] = ix
+    points[i, j, 1] = iy
 
     i += 1
     if i == len(images):
@@ -32,11 +38,11 @@ def onclick(event):
         fig.canvas.mpl_disconnect(cid)
         plt.close()
         transform_images()
-
-    ax.imshow(loaded_images[i])
-    if j > 0:
-        ax.plot(points[i, j - 1, 1], points[i, j - 1, 0], '.', linewidth=7)
-    fig.canvas.draw()
+    else:
+        ax.imshow(loaded_images[i])
+        if j > 0:
+            ax.plot(points[i, j - 1, 0], points[i, j - 1, 1], '.', linewidth=7)
+        fig.canvas.draw()
 
 
 if __name__ == "__main__":
@@ -44,24 +50,30 @@ if __name__ == "__main__":
     images = []
     images += glob.glob(osp.join(folder, '*.jpg'))
     images += glob.glob(osp.join(folder, '*.png'))
-    images = images[:2]
+    images = [im for im in images if 'transformed' not in im]
+    images = sorted(images)
 
     loaded_images = None
     for i, im in enumerate(images):
         load_im = cv2.imread(im)
         if loaded_images is None:
-            loaded_images = np.zeros((len(images),) + load_im.shape, dtype=int)
+            loaded_images = np.zeros((len(images),) + load_im.shape, dtype=np.uint8)
         loaded_images[i] = load_im
-        
+
     n_points = 4
     points = np.zeros((len(images), n_points, 2))
 
-    i = 0
-    j = 0
+    hs_file = osp.join(folder, 'homographies.txt')
+    if osp.isfile(hs_file):
+        hs = np.loadtxt(hs_file)
+        transform_images(hs)
+    else:
+        i = 0
+        j = 0
 
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
 
-    ax.imshow(loaded_images[0])
-    cid = fig.canvas.mpl_connect('button_press_event', onclick)
-    plt.show()
+        ax.imshow(loaded_images[0])
+        cid = fig.canvas.mpl_connect('button_press_event', onclick)
+        plt.show()
